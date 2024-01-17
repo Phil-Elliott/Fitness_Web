e04b60bdb22031a550c234671055f16bcc6fc192
Phil <phillippelliott@gmail.com>
2024-01-17 17:32:16 +0800

    added CalendarButton component and functionality

--- src/components/workouts/header/Header.tsx
+++ src/components/workouts/header/Header.tsx
@@ -1,18 +1,24 @@
+"use client";
+
+import { useState } from "react";
 import { Button } from "@/components/ui/button";
-import {
-  SearchIcon,
-  FilterIcon,
-  PlusIcon,
-  CalendarIcon,
-} from "@/constants/icons";
+import { SearchIcon, FilterIcon, PlusIcon } from "@/constants/icons";
 import Link from "next/link";
+import CalendarButton from "./CalendarButton";
 
 const Header = () => {
+  const [date, setDate] = useState<Date | undefined>(new Date());
+
+  const today = new Date();
+  const displayDate =
+    date?.toDateString() === today.toDateString()
+      ? `Today's Workouts`
+      : date?.toDateString();
+
   return (
     <header className="flex items-center justify-between">
       <div className="text-lg font-semibold sm:text-base mr-4">
-        <h2 className="text-lg font-bold">January 12, 2024</h2>
-        {/* <span className="text-2xl font-bold">{`Today's Workouts`}</span> */}
+        <h2 className="text-lg font-bold">{displayDate}</h2>
       </div>
       <div className="flex items-center gap-4 md:gap-2 lg:gap-4">
         <Button className="rounded-full text-xl" size="icon" variant="ghost">
@@ -23,10 +29,7 @@ const Header = () => {
           <FilterIcon />
           <span className="sr-only">Filter workouts</span>
         </Button>
-        <Button className="rounded-full text-xl" size="icon" variant="ghost">
-          <CalendarIcon />
-          <span className="sr-only">Open calendar</span>
-        </Button>
+        <CalendarButton date={date} setDate={setDate} />
         <Link href="/create-new-workout">
           <Button className="rounded-full text-xl" size="icon" variant="ghost">
             <PlusIcon />
