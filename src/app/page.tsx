3d7bd92eb007dd37f8aab47732c32088f2a564e4
Phil <phillippelliott@gmail.com>
2024-02-16 14:35:54 +0800

    cleaned up and added clerk

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,57 +1,52 @@
-import { Button } from "@/components/ui/button";
-import { CalendarIcon, ClockIcon, FlagIcon } from "@/constants/icons";
-import Link from "next/link";
-import FitnessCard from "@/components/home/FitnessCard";
+"use client";
 
-const fitnessCardsData = [
-  {
-    title: "Current Plan",
-    value: "Full Body Workout",
-    description: "4 weeks program",
-    icon: <CalendarIcon className="w-4 h-4 text-gray-500 dark:text-gray-400" />,
-  },
-  {
-    title: "Next Workout",
-    value: "Back and Chest",
-    description: "Wednesday",
-    icon: <FlagIcon className="w-4 h-4 text-gray-500 dark:text-gray-400" />,
-  },
-  {
-    title: "Recent Workouts",
-    value: "5",
-    description: "Workouts this week",
-    icon: <ClockIcon className="w-4 h-4 text-gray-500 dark:text-gray-400" />,
-  },
-];
+import { Button } from "@/components/ui/button";
+import { SignUpButton, useAuth } from "@clerk/nextjs";
+import { redirect } from "next/navigation";
 
 export default function Home() {
+  const { getToken, isLoaded, isSignedIn } = useAuth();
+
+  if (!isLoaded) {
+    return null;
+  }
+
+  if (isSignedIn) {
+    redirect("/dashboard");
+  }
+
   return (
-    <main className="flex flex-1 flex-col gap-4 py-4 md:gap-8 md:py-10">
-      <section className="flex flex-col items-center justify-center space-y-4 text-center">
-        <h1 className="text-3xl font-bold tracking-tighter sm:text-4xl md:text-5xl lg:text-6xl/none">
-          Your Fitness Journey Starts Here
-        </h1>
-        <p className="max-w-[600px] text-gray-500 md:text-xl/relaxed lg:text-base/relaxed xl:text-xl/relaxed dark:text-gray-400">
-          {`Let's get moving and achieve your fitness goals together!`}
-        </p>
-        <Link href="/create-new-workout">
-          <Button variant="outline">Create a New Workout</Button>
-        </Link>
-      </section>
-      <section className="grid gap-4 md:grid-cols-2 lg:grid-cols-3">
-        {fitnessCardsData.map(({ title, value, description, icon }, i) => (
-          <FitnessCard
-            key={i}
-            title={title}
-            value={value}
-            description={description}
-            icon={icon}
-          />
-        ))}
-      </section>
-    </main>
+    <div className="flex flex-col">
+      <main className="flex-1">
+        <section className="w-full py-6 sm:py-12 md:py-24 lg:py-32 xl:py-32">
+          <div className="container px-4 md:px-6 flex flex-col items-center justify-center space-y-4 text-center">
+            <div className="space-y-6">
+              <h1 className="text-3xl font-bold tracking-tighter sm:text-5xl xl:text-6xl/none">
+                Your Personal Trainer in your Pocket
+              </h1>
+              <p className="mx-auto max-w-[600px] text-gray-500 md:text-xl/relaxed lg:text-base/relaxed xl:text-xl/relaxed dark:text-gray-400">
+                Get fit with our easy-to-use fitness app. Track your workouts,
+                plan your meals, and connect with others on their fitness
+                journey.
+              </p>
+            </div>
+            <div className="flex flex-col gap-2 min-[400px]:flex-row">
+              <SignUpButton>
+                <Button className="px-8">Get Started Today</Button>
+              </SignUpButton>
+            </div>
+          </div>
+        </section>
+      </main>
+    </div>
   );
 }
+
+// export default function Home() {
+//   return (
+//     <main className="flex flex-1 flex-col gap-4 py-4 md:gap-8 md:py-10"></main>
+//   );
+// }
 
 /*
 
@@ -61,4 +56,40 @@ Could make this into a customizable dashboard
 
 might need to add this back in 
 - min-h-[calc(100vh_-_theme(spacing.16))]
+
+https://dribbble.com/shots/22652742-Banking-App-Landing-Page
 */
+
+// const { getToken } = useAuth();
+
+//   useEffect(() => {
+//     const fetchData = async () => {
+//       try {
+//         const token = await getToken({ template: "Testing" });
+//         const response = await fetch("http://localhost:3000/api/v1/exercise", {
+//           method: "GET",
+//           headers: {
+//             "Content-Type": "application/json",
+//             Authorization: `Bearer ${token}`,
+//           },
+//         });
+
+//         if (!response.ok) {
+//           throw new Error("Network response was not ok");
+//         }
+
+//         const result = await response.json();
+//         console.log("result", result);
+//         console.log("token", token);
+
+//         // setData(result);
+//         // setLoading(false);
+//       } catch (err) {
+//         console.error("Error fetching data:", err);
+//         // setError(err);
+//         // setLoading(false);
+//       }
+//     };
+
+//     fetchData();
+//   }, [getToken]);
