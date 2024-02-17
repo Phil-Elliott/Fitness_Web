2fac3083c40996929b35a5de85abe319c69561b1
Phil <phillippelliott@gmail.com>
2024-02-17 13:11:53 +0800

    moved add user fetch to dashboard page

--- src/app/(root)/dashboard/page.tsx
+++ src/app/(root)/dashboard/page.tsx
@@ -2,29 +2,47 @@ import { Button } from "@/components/ui/button";
 import { CalendarIcon, ClockIcon, FlagIcon } from "@/constants/icons";
 import Link from "next/link";
 import FitnessCard from "@/components/home/FitnessCard";
+import { auth, currentUser } from "@clerk/nextjs";
 
-const fitnessCardsData = [
-  {
-    title: "Current Plan",
-    value: "Full Body Workout",
-    description: "4 weeks program",
-    icon: <CalendarIcon className="w-4 h-4 text-gray-500 dark:text-gray-400" />,
-  },
-  {
-    title: "Next Workout",
-    value: "Back and Chest",
-    description: "Wednesday",
-    icon: <FlagIcon className="w-4 h-4 text-gray-500 dark:text-gray-400" />,
-  },
-  {
-    title: "Recent Workouts",
-    value: "5",
-    description: "Workouts this week",
-    icon: <ClockIcon className="w-4 h-4 text-gray-500 dark:text-gray-400" />,
-  },
-];
+async function getData(
+  token: string | null,
+  id: string | undefined,
+  email: string | undefined,
+  username: string | null | undefined
+) {
+  const res = await fetch("http://localhost:3000/api/v1/user", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${token}`,
+    },
+    body: JSON.stringify({
+      clerk_user_id: id,
+      email,
+      display_name: username,
+    }),
+  });
 
-const Dashboard = () => {
+  if (!res.ok) {
+    console.log("Failed to fetch data");
+  }
+
+  console.log("Data fetched successfully");
+
+  return res.json();
+}
+
+const Dashboard = async () => {
+  const { getToken } = auth();
+  const token = await getToken();
+  const user = await currentUser();
+  const data = await getData(
+    token,
+    user?.id,
+    user!.emailAddresses[0].emailAddress,
+    user?.username
+  );
+
   return (
     <div className="flex flex-1 flex-col gap-4 py-4 md:gap-8 md:py-10">
       <section className="flex flex-col items-center justify-center space-y-4 text-center">
@@ -54,3 +72,32 @@ const Dashboard = () => {
 };
 
 export default Dashboard;
+
+/*
+- cache data
+
+- later you can maybe call from the create user to also fetch the dashboard data to be displayed
+
+- maybe add the token to a cookie and then use it to fetch the data through the app
+*/
+
+const fitnessCardsData = [
+  {
+    title: "Current Plan",
+    value: "Full Body Workout",
+    description: "4 weeks program",
+    icon: <CalendarIcon className="w-4 h-4 text-gray-500 dark:text-gray-400" />,
+  },
+  {
+    title: "Next Workout",
+    value: "Back and Chest",
+    description: "Wednesday",
+    icon: <FlagIcon className="w-4 h-4 text-gray-500 dark:text-gray-400" />,
+  },
+  {
+    title: "Recent Workouts",
+    value: "5",
+    description: "Workouts this week",
+    icon: <ClockIcon className="w-4 h-4 text-gray-500 dark:text-gray-400" />,
+  },
+];
