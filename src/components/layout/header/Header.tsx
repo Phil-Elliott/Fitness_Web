2106fa985d88e772e721baf9287fbe9cde865cb5
Phil <phillippelliott@gmail.com>
2024-04-02 12:02:51 +0700

    setup header to handle responsive changes

--- src/components/layout/header/Header.tsx
+++ src/components/layout/header/Header.tsx
@@ -1,18 +1,28 @@
 import DashboardNav from "./DashboardNav";
 import LandingPageNav from "./LandingPageNav";
 import { SignedIn, SignedOut } from "@clerk/nextjs";
+import ResponsiveNav from "./ResponsiveNav";
 
 const Header = () => {
   return (
-    <header className="fixed top-0 left-0 bg-secondary w-full flex lg:justify-between items-center font-semibold py-4 z-50 border-gray-200 border-b">
+    <>
       <SignedOut>
-        <LandingPageNav />
+        <header className="fixed top-0 left-0 bg-secondary w-full flex lg:justify-between items-center font-semibold py-4 z-50 border-gray-200 border-b">
+          <LandingPageNav />
+        </header>
       </SignedOut>
       <SignedIn>
         <DashboardNav />
+        <ResponsiveNav />
       </SignedIn>
-    </header>
+    </>
   );
 };
 
 export default Header;
+
+/*
+
+
+
+*/
