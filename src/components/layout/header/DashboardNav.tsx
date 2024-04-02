aeaf552f5b7533ac5ca5da28638be97fdb38edf7
Phil <phillippelliott@gmail.com>
2024-04-02 12:03:15 +0700

    hid nav on smaller screens

--- src/components/layout/header/DashboardNav.tsx
+++ src/components/layout/header/DashboardNav.tsx
@@ -5,26 +5,28 @@ import { UserButton } from "@clerk/nextjs";
 
 const DashboardNav = () => {
   return (
-    <nav className="container mx-auto flex justify-between items-center">
-      <div className="flex">
-        <Link href="/dashboard" className="flex items-center space-x-2">
-          <FitnessIcon className="text-2xl text-primary" />
-          <p className="text-primary">FitMax</p>
-        </Link>
-        <div className="hidden sm:flex">
-          {headerLinks.map((link) => (
-            <Link
-              href={link.route}
-              className="ml-8 text-primary"
-              key={link.name}
-            >
-              {link.name}
-            </Link>
-          ))}
+    <header className="fixed top-0 left-0 bg-secondary w-full hidden sm:flex lg:justify-between items-center font-semibold py-4 z-50 border-gray-200 border-b">
+      <nav className="container mx-auto flex justify-between items-center">
+        <div className="flex">
+          <Link href="/dashboard" className="flex items-center space-x-2">
+            <FitnessIcon className="text-2xl text-primary" />
+            <p className="text-primary">FitMax</p>
+          </Link>
+          <div className="hidden sm:flex">
+            {headerLinks.map((link) => (
+              <Link
+                href={link.route}
+                className="ml-8 text-primary"
+                key={link.name}
+              >
+                {link.name}
+              </Link>
+            ))}
+          </div>
         </div>
-      </div>
-      <UserButton afterSignOutUrl="/" />
-    </nav>
+        <UserButton afterSignOutUrl="/" />
+      </nav>
+    </header>
   );
 };
 
