79ec7ec9f450ca36ee32457203817a97c4c40aa3
Phil <phillippelliott@gmail.com>
2024-04-03 12:59:37 +0700

    moved add exercise button to its own component

--- src/components/exercises/header/Header.tsx
+++ src/components/exercises/header/Header.tsx
@@ -1,10 +1,8 @@
 "use client";
 
 import { useState } from "react";
-import { Button } from "@/components/ui/button";
-import { SearchIcon, FilterIcon, PlusIcon } from "@/constants/icons";
-import Link from "next/link";
 import CalendarButton from "./CalendarButton";
+import AddExerciseDropdownButton from "./AddExerciseDropdownButton";
 
 const Header = () => {
   const [date, setDate] = useState<Date | undefined>(new Date());
@@ -21,21 +19,8 @@ const Header = () => {
         <h2 className="text-lg font-bold">{displayDate}</h2>
       </div>
       <div className="flex items-center gap-4 md:gap-2 lg:gap-4">
-        {/* <Button className="rounded-full text-xl" size="icon" variant="ghost">
-          <SearchIcon />
-          <span className="sr-only">Search activitys</span>
-        </Button>
-        <Button className="rounded-full text-xl" size="icon" variant="ghost">
-          <FilterIcon />
-          <span className="sr-only">Filter activitys</span>
-        </Button> */}
         <CalendarButton date={date} setDate={setDate} />
-        <Link href="/active-activity">
-          <Button className="rounded-full text-xl" size="icon" variant="ghost">
-            <PlusIcon />
-            <span className="sr-only">Add new activity</span>
-          </Button>
-        </Link>
+        <AddExerciseDropdownButton />
       </div>
     </header>
   );
@@ -44,9 +29,18 @@ const Header = () => {
 export default Header;
 
 /*
+import { Button } from "@/components/ui/button";
 
+import Link from "next/link";
 - Could use a dropdown for the plus icon
       - Could show options (log workout, start new workout, plan workout)
-
+<Button className="rounded-full text-xl" size="icon" variant="ghost">
+          <SearchIcon />
+          <span className="sr-only">Search activitys</span>
+        </Button>
+        <Button className="rounded-full text-xl" size="icon" variant="ghost">
+          <FilterIcon />
+          <span className="sr-only">Filter activitys</span>
+        </Button>
 
 */
