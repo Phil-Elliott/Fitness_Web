e1dc3acef8ac9a12e9d91c36cf6f2d24998b9fb9
Phil <phillippelliott@gmail.com>
2024-04-12 12:50:35 +0700

    fixed zod setup with form

--- src/components/log-exercise/workout/Workout.tsx
+++ src/components/log-exercise/workout/Workout.tsx
@@ -3,7 +3,7 @@
 import React from "react";
 
 import { zodResolver } from "@hookform/resolvers/zod";
-import { useForm } from "react-hook-form";
+import { useForm, useFieldArray } from "react-hook-form";
 import { z } from "zod";
 import {
   Form,
@@ -14,7 +14,6 @@ import {
   FormLabel,
   FormMessage,
 } from "@/components/ui/form";
-import { Label } from "@/components/ui/label";
 import { Input } from "@/components/ui/input";
 import { Textarea } from "@/components/ui/textarea";
 import { Button } from "@/components/ui/button";
@@ -27,21 +26,53 @@ import {
   CardTitle,
 } from "@/components/ui/card";
 import WorkoutDetails from "./WorkoutDetails";
-import { workoutSchema } from "@/schemas/WorkoutSchema";
+import { exercisesSchema, workoutSchema } from "@/schemas";
+import Exercise from "./exercise/Exercise";
+
+const combinedFormSchema = z.object({
+  workout: workoutSchema,
+  exercises: z.array(exercisesSchema),
+});
 
 const Workout = () => {
-  const form = useForm({
-    resolver: zodResolver(workoutSchema),
+  const form = useForm<z.infer<typeof combinedFormSchema>>({
+    resolver: zodResolver(combinedFormSchema),
     defaultValues: {
-      name: "",
-      notes: "",
-      date: new Date().toISOString().slice(0, 10),
+      workout: {
+        name: "",
+        date: new Date().toISOString().slice(0, 10),
+        notes: "",
+      },
+      exercises: [
+        {
+          name: "",
+          description: "",
+          sets: [
+            {
+              set_number: 1,
+              repetitions: 0,
+              weight: 0,
+              weight_unit: "lbs",
+              notes: "",
+            },
+          ],
+        },
+      ],
     },
   });
 
-  function onSubmit(values: z.infer<typeof workoutSchema>) {
+  const {
+    fields: exerciseFields,
+    append: appendExercise,
+    remove: removeExercise,
+  } = useFieldArray({
+    control: form.control,
+    name: "exercises",
+  });
+
+  const onSubmit = (values: z.infer<typeof combinedFormSchema>) => {
     console.log(values);
-  }
+  };
 
   return (
     <Form {...form}>
@@ -49,9 +80,22 @@ const Workout = () => {
         <Card>
           <CardContent className="space-y-2 pt-4">
             <WorkoutDetails form={form} />
+            {exerciseFields.map((field, index) => (
+              <Exercise
+                key={field.id}
+                form={form}
+                index={index}
+                onRemove={() => removeExercise(index)}
+              />
+            ))}
           </CardContent>
-          <CardFooter>
-            <Button type="submit">Submit</Button>
+          <CardFooter className="flex flex-col gap-4">
+            <Button className="w-full" type="button" variant="outline">
+              Add Exercise
+            </Button>
+            <Button className="w-full" type="submit">
+              Submit
+            </Button>
           </CardFooter>
         </Card>
       </form>
@@ -60,3 +104,23 @@ const Workout = () => {
 };
 
 export default Workout;
+
+/*
+
+change exercise component
+- make it into a form
+- gotta change the inputs
+- button to add set
+- maybe need a set component as well
+- button to remove set
+- button to remove exercise
+
+
+
+when done
+- clean up the form
+- delete schemas you dont need
+- hook up to backend
+- might need to make a special handler for this
+
+*/
